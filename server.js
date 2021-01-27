bfbce860da3dd53c095a5ae35bc7ecc494c3aab1
km Habib <31393442+kmhabib71@users.noreply.github.com>
2021-01-27 05:56:52 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '')));
 
-server = app.listen(3000);
+server = app.listen(process.env.PORT || 5000);
 
 const io = require("socket.io")(server);
 
