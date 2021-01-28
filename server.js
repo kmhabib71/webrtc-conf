eeaf97c78b0748d18e29cc0f11dd7a68c355f540
unknown <km.habibs@gmail.com>
2021-01-27 19:22:06 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const socketio = require('socket.io');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '')));
+
+server = app.listen(3000);
+
+const io = require("socket.io")(server);
+
+const PORT = 3000 || process.env.PORT;
+console.log(`Server running on port ${PORT}`);
+var userConnection = [];
+
+io.on('connection', (socket) => {
+    socket.on('users_info_to_signaling_server', (data) => {
+        var other_users = userConnection.filter(p => p.meeting_id == data.meetingid);
+        userConnection.push({
+            connectionId: socket.id,
+            user_id: data.current_user_name,
+            meeting_id: data.meetingid
+        })
+
+        console.log(`all users: ${userConnection.map(a=>a.connectionId)}`);
+        console.log(`other users: ${other_users.map(a=>a.connectionId)}`);
+
+        other_users.forEach(v => {
+            socket.to(v.connectionId).emit('other_users_to_inform', {
+                other_user_id: data.current_user_name,
+                connId: socket.id
+            })
+        });
+        socket.emit('newConnectionInformation', other_users);
+
+    })
+    socket.on('sdpProcess', (data) => {
+        socket.to(data.to_connid).emit('sdpProcess', {
+            message: data.message,
+            from_connid: socket.id
+        })
+    })
+
+    socket.on("disconnect", function () {
+        var disUser = userConnection.find(p => p.connectionId == socket.id);
+        if (disUser) {
+            var meetingid = disUser.meeting_id;
+            userConnection = userConnection.filter(p => p.connectionId != socket.id);
+            var restUser = userConnection.filter(p => p.meeting_id == meetingid);
+            restUser.forEach(m => {
+                socket.to(m.connectionId).emit('closedConnectionInfo', socket.id);
+            })
+
+        }
+    })
+
+
+
+})
